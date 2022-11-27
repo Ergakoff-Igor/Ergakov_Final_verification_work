6b2a1679acedc2ff2b8b8e0e41fd53c7dbfa7999
Igor <i.ergakoff@mail.ru>
2022-11-27 21:59:54 +0300

    Объявили два массива, составили метод вывода массива

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] InputArray = new string[4] {"hello", "2", "world", ":-)"};
+string[] ResultArray = new string[InputArray.Length];
+Console.WriteLine("Изначальный массив: ");
+PrintArray(InputArray);
+
+//Метод вывода массива:
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i == 0) Console.Write($"[{array[i]}, ");
+        if (i < array.Length-1) Console.Write($"{array[i]}, ");
+        if (i == array.Length-1) Console.Write($"{array[i]}] ");
+    }
+    Console.WriteLine();
+}
